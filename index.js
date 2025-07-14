d17f5df551c1575c8607d07656e70546ed9b1365
Saima Imran <smalldeeds1234@gmail.com>
2025-07-15 01:28:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const url = require('url');
 
-// Dummy in-memory array to store todos
+
 let todos = [
   { id: 1, text: 'First todo' }
 ];
 
 
-// Helper: parse request body
+
 function parseRequestBody(req, callback) {
   let body = '';
   req.on('data', chunk => body += chunk);
@@ -21,22 +21,22 @@ function parseRequestBody(req, callback) {
   });
 }
 
-// Create server
+
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const method = req.method;
   const path = parsedUrl.pathname;
-  const parts = path.split('/').filter(Boolean); // ['todos', '1'] etc.
+  const parts = path.split('/').filter(Boolean); 
 
   res.setHeader('Content-Type', 'application/json');
 
-  // GET /todos - show all todos
+  
   if (method === 'GET' && path === '/todos') {
     res.writeHead(200);
     res.end(JSON.stringify(todos));
   }
 
-  // POST /todos - add new todo
+ 
   else if (method === 'POST' && path === '/todos') {
     parseRequestBody(req, data => {
       const newTodo = {
@@ -49,7 +49,7 @@ const server = http.createServer((req, res) => {
     });
   }
 
-  // PUT /todos/:id - edit todo
+  
   else if (method === 'PUT' && parts[0] === 'todos' && parts[1]) {
     const id = parseInt(parts[1]);
     const todo = todos.find(t => t.id === id);
@@ -65,7 +65,7 @@ const server = http.createServer((req, res) => {
     });
   }
 
-  // DELETE /todos/:id - delete todo
+
   else if (method === 'DELETE' && parts[0] === 'todos' && parts[1]) {
     const id = parseInt(parts[1]);
     const index = todos.findIndex(t => t.id === id);
